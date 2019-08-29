713632f78bea9e58af1599ef93580f63d92431b3
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-08-29 16:34:23 -0400

    removes original xcode project folder, folder was broken. adds new xcode project. adds jokes json file

---
+++ iOS-Introduction-to-Unit-Testing-Lab/iOS-Introduction-to-Unit-Testing-Lab/View/jokesJSON.swift
@@ -0,0 +1,9 @@
+//
+//  jokesJSON.swift
+//  iOS-Introduction-to-Unit-Testing-Lab
+//
+//  Created by Mariel Hoepelman on 8/29/19.
+//  Copyright © 2019 Mariel Hoepelman. All rights reserved.
+//
+
+import Foundation
